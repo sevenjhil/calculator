e76c1943ad598f6a460817124cd42f1bbbd01258
alexisholyoak <peraltaholyoak.aj@gmail.com>
2019-04-18 09:18:39 -0500

    function plus

---
+++ calculator.php
@@ -0,0 +1,10 @@
+<?php
+	$operando1 = $_GET['operando1'];
+	$operando2 = $_GET['operando2'];
+	$operador = $_GET['operador'];
+	
+	if($operador == "+"){
+		$solucion = $operando1 + $operando2;
+	}
+	echo "La solución es: ".$solucion;
+?>
